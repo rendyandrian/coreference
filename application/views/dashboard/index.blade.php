b9bb82fe99c8b6e355a69116eefd238fe54381e0
Rendy Andrian Saputra <srendyandrian@gmail.com>
2020-03-12 16:15:44 +0700

    [rendy] remove title

--- application/views/dashboard/index.blade.php
+++ application/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 	<h1 class="h3 mb-4 text-gray-800">Welcome</h1>
 	<!-- Content Row -->
 	<div class="row">
-		<p>Selamat datang di aplikasi e-meeting</p>
+		<p>Selamat datang di aplikasi Coreference Resolution</p>
 	</div>
 
 	<!-- Content Row -->
